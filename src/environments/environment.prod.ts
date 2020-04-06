be43bf4b88b82ffd2f9f01416998f3bca69056fc
ahmedkamel095 <ahmedkamel095@gmail.com>
2020-04-07 00:25:44 +0200

    Initialized app and added firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAnyY6X3uWLroTHLsGNJPjWn5iBlDbY4Jg',
+    authDomain: 'angular-http-3da85.firebaseapp.com',
+    databaseURL: 'https://angular-http-3da85.firebaseio.com',
+    projectId: 'angular-http-3da85',
+    storageBucket: 'angular-http-3da85.appspot.com',
+    messagingSenderId: '561154879345',
+    appId: '1:561154879345:web:9d28e329e4e24fa74050d0',
+    measurementId: 'G-VT5VWWCFKF',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAnyY6X3uWLroTHLsGNJPjWn5iBlDbY4Jg',
+    authDomain: 'angular-http-3da85.firebaseapp.com',
+    databaseURL: 'https://angular-http-3da85.firebaseio.com',
+    projectId: 'angular-http-3da85',
+    storageBucket: 'angular-http-3da85.appspot.com',
+    messagingSenderId: '561154879345',
+    appId: '1:561154879345:web:9d28e329e4e24fa74050d0',
+    measurementId: 'G-VT5VWWCFKF',
+  },
 };
 
 /*
